da60351466cf362724aa17f3781d809685bbe15b
nn <nn>
2025-02-04 07:04:57 -0500

    feat: :sparkles: add navbar

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import { MainMenu } from "./components/mainMenu.js";
+
+
+document.querySelectorAll(".nav-link").forEach((val,id) => {
+    val.addEventListener("click", (e)=>{
+        let data =JSON.parse(e.target.dataset.verocultar);
+        let cardVer = document.querySelector(data[0]);
+        cardVer.style.display ='block';
+        data[1].forEach(card => {
+            let cardActual = document.querySelector(card);
+            cardActual.style.display = 'none';
+        });
+        e.stopImmediatePropagation();
+         e.preventDefault();
+
+    })
+});
